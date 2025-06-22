f6974b7a401daca209e39f0c555df395fab93bf0
Rauls1306 <rauleduardosm@gmail.com>
2025-06-22 20:23:32 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -131,4 +131,3 @@ from datetime import datetime
     filename = f"/tmp/articulo_{datetime.now().strftime('%Y%m%d%H%M%S')}.docx"
     save_article_to_docx(final_article, filename)
     return filename
-
