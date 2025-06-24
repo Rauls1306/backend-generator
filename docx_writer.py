f30ce51089e119b19ab45cd410600eef4dde0a96
Rauls1306 <rauleduardosm@gmail.com>
2025-06-24 02:40:52 +0200

    Update docx_writer.py

--- docx_writer.py
+++ docx_writer.py
@@ -1,7 +1,10 @@
 from docx import Document
+from docx.shared import Pt
 
 def save_article_to_docx(texto, ruta_salida):
     doc = Document()
     for parrafo in texto.split("\n\n"):
-        doc.add_paragraph(parrafo.strip())
+        if parrafo.strip():
+            p = doc.add_paragraph(parrafo.strip())
+            p.style.font.size = Pt(11)
     doc.save(ruta_salida)
